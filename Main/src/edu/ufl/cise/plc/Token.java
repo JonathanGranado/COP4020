c4921d34394681933884c7a6e591a9b3c9179093
gabcoroba <gabcoroba@gmail.com>
2022-01-24 22:06:28 -0500

    Adds map of reserved words and a method to check if token is reserved word

--- Main/src/edu/ufl/cise/plc/Token.java
+++ Main/src/edu/ufl/cise/plc/Token.java
@@ -1,7 +1,54 @@
 package edu.ufl.cise.plc;
-
+import java.util.*;
 public class Token implements IToken {
 
+
+    Map<String, Kind> reservedMap = new HashMap<>();
+
+    // Inserting reserved words into map
+
+    public Map<String, Kind> getReservedMap() {
+        // <type>
+        reservedMap.put("int", Kind.TYPE);
+        reservedMap.put("float", Kind.TYPE);
+        reservedMap.put("string", Kind.TYPE);
+        reservedMap.put("boolean", Kind.TYPE);
+        reservedMap.put("color", Kind.TYPE);
+        reservedMap.put("image", Kind.TYPE);
+        reservedMap.put("void", Kind.KW_VOID);
+        // <image_op>
+        reservedMap.put("getWidth", Kind.IMAGE_OP);
+        reservedMap.put("getHeight", Kind.IMAGE_OP);
+        // <color_op>
+        reservedMap.put("BLACK", Kind.COLOR_CONST);
+        reservedMap.put("BLUE", Kind.COLOR_CONST);
+        reservedMap.put("CYAN", Kind.COLOR_CONST);
+        reservedMap.put("DARK_GRAY", Kind.COLOR_CONST);
+        reservedMap.put("GRAY", Kind.COLOR_CONST);
+        reservedMap.put("GREEN", Kind.COLOR_CONST);
+        reservedMap.put("LIGHT_GRAY", Kind.COLOR_CONST);
+        reservedMap.put("MAGENTA", Kind.COLOR_CONST);
+        reservedMap.put("ORANGE", Kind.COLOR_CONST);
+        reservedMap.put("PINK", Kind.COLOR_CONST);
+        reservedMap.put("RED", Kind.COLOR_CONST);
+        reservedMap.put("WHITE", Kind.COLOR_CONST);
+        reservedMap.put("YELLOW", Kind.COLOR_CONST);
+        // <boolean_lit>
+        reservedMap.put("true", Kind.BOOLEAN_LIT);
+        reservedMap.put("false", Kind.BOOLEAN_LIT);
+        // <other_keyword>
+        reservedMap.put("if", Kind.KW_IF);
+        reservedMap.put("fi", Kind.KW_FI);
+        reservedMap.put("else", Kind.KW_ELSE);
+        reservedMap.put("write", Kind.KW_WRITE);
+        reservedMap.put("console", Kind.KW_WRITE);
+
+        return reservedMap;
+    }
+
+    public boolean isReserved(String str){
+        return reservedMap.containsKey(str);
+    }
     @Override
     public Kind getKind() {
         return null;
