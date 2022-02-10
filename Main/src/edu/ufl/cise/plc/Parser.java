7d3e787a0c313feb3d4922b6fa3ca628abdf001b
Jonathan Granado <84871765+JonathanGranado@users.noreply.github.com>
2022-02-10 00:07:58 -0500

    Adding necessary files and module for Parser

---
+++ Main/src/edu/ufl/cise/plc/Parser.java
@@ -0,0 +1,13 @@
+package edu.ufl.cise.plc;
+
+import edu.ufl.cise.plc.ast.ASTNode;
+
+public class Parser implements IParser {
+    public Parser(String input) {
+    }
+
+    @Override
+    public ASTNode parse() throws PLCException {
+        return null;
+    }
+}
