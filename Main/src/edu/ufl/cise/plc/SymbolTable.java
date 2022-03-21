91d9089c3746edf187b410723ca8a7d8c337befe
gabcoroba <gabcoroba@gmail.com>
2022-03-20 21:31:16 -0400

    Adding SymbolTable skeleton

--- Main/src/edu/ufl/cise/plc/SymbolTable.java
+++ Main/src/edu/ufl/cise/plc/SymbolTable.java
@@ -1,7 +1,25 @@
 package edu.ufl.cise.plc;
 
+import edu.ufl.cise.plc.ast.ASTNode;
+import edu.ufl.cise.plc.ast.Declaration;
+
+import java.util.HashMap;
+
 public class SymbolTable {
 
-//TODO:  Implement a symbol table class that is appropriate for this language. 
+    //TODO:  Implement a symbol table class that is appropriate for this language.
+    HashMap<String, Declaration> table = new HashMap<>();
+    SymbolTable symbolTable = new SymbolTable();
+
+    public boolean insert(String name, Declaration declaration) {
+        return (table.putIfAbsent(name, declaration) == null);
+    }
+
+    public Declaration lookup(String name) {
+        return table.get(name);
+    }
+
 
 }
+
+
--- Main/src/edu/ufl/cise/plc/ast/Declaration.java
+++ Main/src/edu/ufl/cise/plc/ast/Declaration.java
@@ -3,7 +3,7 @@ package edu.ufl.cise.plc.ast;
 import edu.ufl.cise.plc.IToken;
 import edu.ufl.cise.plc.ast.Types.Type;
 
-public abstract class Declaration extends ASTNode {
+public abstract class  Declaration extends ASTNode {
 
 	public Declaration(IToken firstToken) {
 		super(firstToken);
