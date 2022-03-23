12235284c00a5857734fc14e89284ca0a1414284
gabcoroba <gabcoroba@gmail.com>
2022-03-22 23:32:17 -0400

    Fixed binaryExpr, passes all but 1 test

--- Main/src/edu/ufl/cise/plc/TypeCheckException.java
+++ Main/src/edu/ufl/cise/plc/TypeCheckException.java
@@ -5,21 +5,21 @@ import edu.ufl.cise.plc.IToken.SourceLocation;
 @SuppressWarnings("serial")
 public class TypeCheckException extends PLCException {
 
-	
-	public TypeCheckException(String message) {
-		super(message);
-	}
-	
-	public TypeCheckException(Throwable cause) {
-		super(cause);
-	}
-	
-	public TypeCheckException(String message, SourceLocation loc) {
-		super(message, loc);
-	}
-	
-	public TypeCheckException(String error_message, int line, int column) {
-		super(line + ":" + column + "  " + error_message);
-	}
+
+    public TypeCheckException(String message) {
+        super(message);
+    }
+
+    public TypeCheckException(Throwable cause) {
+        super(cause);
+    }
+
+    public TypeCheckException(String message, SourceLocation loc) {
+        super(message, loc);
+    }
+
+    public TypeCheckException(String error_message, int line, int column) {
+        super(line + ":" + column + "  " + error_message);
+    }
 
 }
